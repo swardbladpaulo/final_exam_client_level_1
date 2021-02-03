1fe12c4b72474a8a0c511dce72ccacbffabe8ad1
swardbladpaulo <paulo@swardblads.se>
2021-02-03 13:09:45 +0100

    add baseUrl to cypress.json, add axios package, add react-redux package, add list_of_series_response.json fixture, add first test for displayListOfSeries

---
+++ cypress/integration/displayListOfSeries.feature.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+describe('display list of series', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'https://content.viaplay.se/pc-se/serier/samtliga',
+      response: 'fx:list_of_series_response.json',
+    })
+    cy.visit('/')
+  })
+
+  it('successfully view all series', () => {
+    cy.get('[data-cy= "index"]').within(() => {
+      cy.get(">div").each((_, index) => {
+        cy.get(`[data-cy= "index-${index + 1}"]`).within(() => {
+          cy.get('[data-cy= "image"]').should("exist");
+        })
+      })
+    });
+  });
+
+
+
+
+
+
+})
