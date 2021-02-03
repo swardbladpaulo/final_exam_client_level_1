4e5688a484629ec7515bc50bcb3769eb8a1a80cf
swardbladpaulo <paulo@swardblads.se>
2021-02-03 13:24:04 +0100

    add styling to header in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <div id="header" data-cy="header">
+        <img id="logo" data-cy="logo" src="https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg"></img>
+      </div>
     </div>
   )
 }
