5f5472034dfe64d46efd9c9ff7a508e158c013d4
swardbladpaulo <paulo@swardblads.se>
2021-02-03 13:34:29 +0100

    add footer to the page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
       <div id="header" data-cy="header">
         <img id="logo" data-cy="logo" src="https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg"></img>
       </div>
+      <div id="footer" data-cy="footer">
+      </div>
     </div>
   )
 }
